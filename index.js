1f9a432a6b8b63f5d5ca97ff257e61d45d795477
Shawn McNaughton <shawn@dev.hitoma.com>
2017-06-19 20:38:03 +0100

    Adding main script file

---
+++ index.js
@@ -0,0 +1,6 @@
+var Promise = require('bluebird');
+var _ = require('lodash');
+var PostalCodes = require('postal-codes');
+var FeedParser = require('feedparser');
+var PolygonsIntersect = require('polygons-intersect');
+
